23e67918b4cc24afb80409ef83b47dc54c61de70
NelsonV362 <nvillalta54@gmail.com>
2025-07-11 23:14:56 -0500

    Esta todo listo

--- src/App.java
+++ src/App.java
@@ -27,12 +27,35 @@ public class App {
         mapa.runTreeMapObj();
     }
 
-    private static void runEmpleadoExample() {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
     private static void runEjerccios() {
-        throw new UnsupportedOperationException("Not implemented yet");
+    Ejercicios ejercicios = new Ejercicios();
 
-    }
+    System.out.println("\n--- Anagramas ---");
+    System.out.println("listen vs silent: " + Ejercicios.areAnagrams("listen", "silent")); 
+    System.out.println("hello vs bello: " + Ejercicios.areAnagrams("hello", "bello"));     
+    System.out.println("triangle vs integral: " + Ejercicios.areAnagrams("triangle", "integral")); 
+
+    System.out.println("\n--- Sumatoria de Dos ---");
+    int[] resultado1 = ejercicios.sumatoriaDeDos(new int[]{9, 2, 3, 6}, 5);
+    if (resultado1 != null)
+        System.out.println("Índices para 5: [" + resultado1[0] + ", " + resultado1[1] + "]");
+    else
+        System.out.println("No hay combinación para 5");
+
+    int[] resultado2 = ejercicios.sumatoriaDeDos(new int[]{9, 2, 3, 6}, 10);
+    if (resultado2 != null)
+        System.out.println("Índices para 10: [" + resultado2[0] + ", " + resultado2[1] + "]");
+    else
+        System.out.println("No hay combinación para 10");
+
+    System.out.println("\n--- Contar Caracteres ---");
+    ejercicios.contarCaracteres("hola"); 
+
+    // Prueba de sonAnagramas
+    System.out.println("\n--- sonAnagramas ---");
+    System.out.println("roma vs amor: " + ejercicios.sonAnagramas("roma", "amor")); 
+    System.out.println("roma vs ramo: " + ejercicios.sonAnagramas("roma", "ramo")); 
+}
+
+    
 }
